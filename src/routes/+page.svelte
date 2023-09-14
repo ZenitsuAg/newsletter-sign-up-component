c4a85295cf89faf76d10b5c435e3e91c31be48a6
Fred Robins <fredrobins619@outlook.com>
2023-09-14 22:58:59 +0000

    Install TailwindCSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,17 @@
-<script>
+<!-- <script>
     import Button from './Nested/Button.svelte'
 </script>
 
 <main>
     <Button />
-</main>
+</main> -->
+
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  
+  <style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.100);
+    }
+  </style>
