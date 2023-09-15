6daf591fd435d8d1dff156adf3f476091ab0642d
Fred Robins <fredrobins619@outlook.com>
2023-09-15 13:49:17 +0000

    Change from svelte to react

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,20 @@
 module.exports = {
-	root: true,
-	extends: ['eslint:recommended', 'plugin:svelte/recommended', 'prettier'],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	}
-};
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
--- src/routes/Nested/Button.svelte
+++
@@ -1,6 +0,0 @@
-
-<script>
-
-</script>
-
-<button>Submit</button>
