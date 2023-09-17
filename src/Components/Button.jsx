b4ace63a35600c6f81fd6c3f597df86f9e224e5e
Fred Robins <fredrobins619@outlook.com>
2023-09-17 20:12:30 +0000

    Update Button

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,8 +1,9 @@
-export default function Button({ text, disabled }) {
+export default function Button({ text, disabled, onClick }) {
     return (
         <button
-        className="p-3 border border-green-500 rounded hover:bg-green-500 hover:text-white disabled:bg-slate-400"
+        className="p-3 text-green-500 border border-green-500 rounded hover:bg-green-500 hover:text-white disabled:bg-slate-900 disabled:text-white"
         disabled={disabled}
+        onClick={onClick}
         >{ text }</button>
     )
 }
