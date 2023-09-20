ab80face01398fca96470e3cf09002e950cc870f
Fred Robins <fredrobins619@outlook.com>
2023-09-20 09:40:23 +0000

    SuccessMsg desktop design

--- src/Components/SuccessMsg.jsx
+++ src/Components/SuccessMsg.jsx
@@ -6,25 +6,27 @@ function SuccessMsg({ onDismiss }) {
     const { email } = useContext(AppContext);
 
     return (
-        <div className="flex flex-col justify-around h-screen p-5">
-            <div>
-                <img src="./assets/images/icon-success.svg" 
-                alt="success-icon" 
-                className="max-w-[150px] self-start mb-9"
-                />
-                <h1 className="text-4xl font-bold text-dark-slate-grey">
-                    Thanks for subscribing!
-                </h1>
-                <p className="text-charcoal-grey">
-                    A confirmation email has been sent to <strong>{email}. </strong>  
-                    Please open it and click the button inside to confirm your subscription.
-                </p>
-            </div>
-            <div>
-                <Button type={'reset'} text={'Dismiss message'} onClick={onDismiss}/>
-            </div>
+       <div className="flex items-center justify-center h-screen md:bg-charcoal-grey">
+            <div className="flex flex-col h-screen justify-around md:justify-center md:rounded-3xl md:p-10 p-5 max-w-[375px] md:max-h-[375px] md:bg-white">
+                <div className="">
+                    <img src="./assets/images/icon-success.svg" 
+                    alt="success-icon" 
+                    className="max-w-[150px] md:max-w-[50px] self-start mb-9 md:mb-3"
+                    />
+                    <h1 className="mb-3 text-4xl font-bold text-dark-slate-grey">
+                        Thanks for subscribing!
+                    </h1>
+                    <p className="text-sm text-charcoal-grey">
+                        A confirmation email has been sent to <strong>{email}. </strong>  
+                        Please open it and click the button inside to confirm your subscription.
+                    </p>
+                </div>
+                <div>
+                    <Button type={'reset'} text={'Dismiss message'} onClick={onDismiss}/>
+                </div>
             
-        </div>
+            </div>
+       </div>
     )
 }
 
