aa80b0c5a1bee483a8d114fc4e6fb3ffb877d792
Fred Robins <fredrobins619@outlook.com>
2023-09-20 08:42:09 +0000

    App mobile logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-// import { useState } from 'react'
-import SignUp from "./Components/SignUp"
-import SucessMsg from "./Components/SuccessMsg"
+import { useState } from "react";
+import { AppContext } from "./Components/AppContext";
+import SignUp from "./Components/SignUp";
+import SuccessMsg from "./Components/SuccessMsg";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [emailValid, setEmailValid] = useState(false)
+
+  function handleEmailValidation() {
+      setEmailValid(true);
+  };
 
   return (
-    <main>
-      <SignUp />
-    </main>
-  )
+      <main>
+          <AppContext.Provider value={{ email, setEmail }}>
+              <div className="App font-roboto">
+                  {/* {email === 'a@b.com' ? <SignUp /> : <SuccessMsg />} */}
+                  {emailValid ? (
+                      <SuccessMsg />
+                    ) : (
+                      <SignUp onEmailValidation={handleEmailValidation} />
+                    )}
+              </div>
+            </AppContext.Provider>
+      </main>
+  );
 }
 
-export default App
+export default App;
