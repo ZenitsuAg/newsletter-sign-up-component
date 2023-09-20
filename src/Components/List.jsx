8a71938edc3f525bbffeaf755518109e857e4e53
Fred Robins <fredrobins619@outlook.com>
2023-09-20 20:38:58 +0000

    Image path modified

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,8 +1,9 @@
+import iconSuccess from './../../assets/images/icon-success.svg'
+
 export default function List ({ text }) {
     return (
         <div className="flex items-center gap-4 mb-2">
-            <img src="./../../assets/images/icon-success.svg" alt="success-icon" className="max-w-[20px] self-start"/>
-            {/* <img src={iconSuccess} alt="success-icon" className="max-w-[20px] self-start" /> */}
+            <img src={iconSuccess} alt="success-icon" className="max-w-[20px] self-start"/>
             <p>{text}</p>
         </div>
     )
