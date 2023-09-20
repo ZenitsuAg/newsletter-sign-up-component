5df34ecc0119a15668ef11f500c3b23b2c589935
Fred Robins <fredrobins619@outlook.com>
2023-09-20 20:24:14 +0000

    Change image path

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,7 +1,8 @@
 export default function List ({ text }) {
     return (
         <div className="flex items-center gap-4 mb-2">
-            <img src="./assets/images/icon-success.svg" alt="success-icon" className="max-w-[20px] self-start"/>
+            <img src="./../../assets/images/icon-success.svg" alt="success-icon" className="max-w-[20px] self-start"/>
+            {/* <img src={iconSuccess} alt="success-icon" className="max-w-[20px] self-start" /> */}
             <p>{text}</p>
         </div>
     )
