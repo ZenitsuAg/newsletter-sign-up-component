5ec64edca5f1b996bdf64a7fdb015c49400ad721
Fred Robins <fredrobins619@outlook.com>
2023-09-20 08:40:54 +0000

    SuccessMsg mobile design

--- src/Components/SuccessMsg.jsx
+++ src/Components/SuccessMsg.jsx
@@ -1,3 +1,31 @@
-export default function SucessMsg() {
-    
+import { useContext } from "react"
+import { AppContext } from "./AppContext"
+import Button from "./Button"
+
+function SuccessMsg({ onDismiss }) {
+    const { email } = useContext(AppContext);
+
+    return (
+        <div className="flex flex-col justify-around h-screen p-5">
+            <div>
+                <img src="./assets/images/icon-success.svg" 
+                alt="success-icon" 
+                className="max-w-[150px] self-start mb-9"
+                />
+                <h1 className="text-4xl font-bold text-dark-slate-grey">
+                    Thanks for subscribing!
+                </h1>
+                <p className="text-charcoal-grey">
+                    A confirmation email has been sent to <strong>{email}. </strong>  
+                    Please open it and click the button inside to confirm your subscription.
+                </p>
+            </div>
+            <div>
+                <Button type={'reset'} text={'Dismiss message'} onClick={onDismiss}/>
+            </div>
+            
+        </div>
+    )
 }
+
+export default SuccessMsg
